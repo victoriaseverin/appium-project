d3cfc350988620803dc7f1a09aa4eb3ea3426376
victoriasseverin <victoriasseverin@gmail.com>
2025-01-21 21:51:31 -0800

    removing test

--- src/test/java/appiumproject/Appium/BaseTest.java
+++ src/test/java/appiumproject/Appium/BaseTest.java
@@ -46,7 +46,6 @@ public class BaseTest {
 		driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options); 
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));		
-		//test
 		
 	}
 	
